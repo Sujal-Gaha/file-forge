b5dbac4647cd58f329ca371cc1610a12a9140a52
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-10-15 11:52:23 +0545

    video conversion module

--- file_forge/converters/video.py
+++ file_forge/converters/video.py
@@ -9,13 +9,13 @@ class VideoProcessor:
         self.output_path = output_path
 
     def convert(self, output_format: str, quality: int = 95) -> Path:
-        output_format = output_format.lower().replace(".", "") 
-                    
+        output_format = output_format.lower().replace(".", "")
+
         if self.output_path is None:
             self.output_path = self.input_path.with_suffix(f".{output_format}")
-            
+
         self.output_path = self.output_path.resolve()
-            
+
         with VideoFileClip(filename=self.input_path) as clip:
             clip.write_videofile(
                 filename=self.output_path,
@@ -24,7 +24,7 @@ class VideoProcessor:
                 preset="veryslow",
                 audio_codec="aac",
                 audio_bitrate="192k",
-                logger=None
+                logger=None,
             )
-            
+
         return self.output_path
